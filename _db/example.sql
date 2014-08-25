19a37fbb7ab2aaf3efb839e0b4444355b686a1cb
coronellw <coronell@gmail.com>
2014-08-25 00:02:02 -0500

    db update

--- _db/example.sql
+++ _db/example.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `resources` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */;
 USE `resources`;
--- MySQL dump 10.13  Distrib 5.5.38, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
 --
 -- Host: 127.0.0.1    Database: resources
 -- ------------------------------------------------------
--- Server version	5.5.38-0ubuntu0.14.04.1
+-- Server version	5.6.16
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,6 +18,37 @@ USE `resources`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `contactos`
+--
+
+DROP TABLE IF EXISTS `contactos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contactos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `numero` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `persona` int(11) NOT NULL,
+  `tipo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_persona_idx` (`persona`),
+  KEY `fk_c_tipo_idx` (`tipo`),
+  CONSTRAINT `fk_c_persona` FOREIGN KEY (`persona`) REFERENCES `personas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_c_tipo` FOREIGN KEY (`tipo`) REFERENCES `tipos_contacto` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contactos`
+--
+
+LOCK TABLES `contactos` WRITE;
+/*!40000 ALTER TABLE `contactos` DISABLE KEYS */;
+INSERT INTO `contactos` VALUES (1,'5528317956',1,1),(2,'5512544075',1,2),(3,'jsnow',1,3);
+/*!40000 ALTER TABLE `contactos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `personas`
 --
 
@@ -26,10 +57,11 @@ DROP TABLE IF EXISTS `personas`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `personas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombres` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
-  `apellidos` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `nombres` varchar(75) COLLATE utf8_spanish_ci NOT NULL,
+  `apellidos` varchar(75) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `observaciones` varchar(250) COLLATE utf8_spanish_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
+  UNIQUE KEY `idpersonas_UNIQUE` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -39,8 +71,33 @@ CREATE TABLE `personas` (
 
 LOCK TABLES `personas` WRITE;
 /*!40000 ALTER TABLE `personas` DISABLE KEYS */;
-INSERT INTO `personas` VALUES (1,'Winston','Coronell Garcia'),(2,'Alexander','Baena Ortiz'),(3,'Rodrigo','Iglesias'),(4,'Estefania','Veliz Cavanzo'),(5,'Karen','Lucciani'),(6,'Laura','Cabana');
+INSERT INTO `personas` VALUES (1,'Jon','Snow','Lord commander of night\'s watch'),(2,'Cersei','Lannister','Bitch'),(3,'Sam','Tarly','Trusted friend'),(4,'Theon','Greyjoy','Reek'),(5,'Daenerys','Targaryen','Mother of dragons'),(6,'Tyrion','Lannister','Smart man');
 /*!40000 ALTER TABLE `personas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tipos_contacto`
+--
+
+DROP TABLE IF EXISTS `tipos_contacto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tipos_contacto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipos_contacto`
+--
+
+LOCK TABLES `tipos_contacto` WRITE;
+/*!40000 ALTER TABLE `tipos_contacto` DISABLE KEYS */;
+INSERT INTO `tipos_contacto` VALUES (1,'Casa'),(2,'Celular'),(3,'Facebook'),(4,'Twitter'),(5,'Google+');
+/*!40000 ALTER TABLE `tipos_contacto` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -52,4 +109,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-08-22 21:09:47
+-- Dump completed on 2014-08-25  0:00:15
